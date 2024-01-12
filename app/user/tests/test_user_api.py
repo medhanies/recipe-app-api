1b585430abaf1d2b520bd95b383790589361f3da
medhanies <medhaniesolomon@yahoo.com>
2024-01-11 18:20:39 -0600

    create user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,9 +32,9 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HHTP_201_CREATED)
-        user = get_user_model().objects.get(email=payload('email'))
-        self.assertTrue(user.check_password(payload('password')))
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
     def test_user_with_email_exists_error(self):
@@ -49,7 +49,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_passwod_too_short_error(self):
+    def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars"""
         payload = {
             'email': 'test@example.com',
