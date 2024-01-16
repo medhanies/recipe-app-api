1ef5fc7d261abb115a3e568e788a5aa3db1c0eff
medhanies <medhaniesolomon@yahoo.com>
2024-01-15 19:16:30 -0600

    remove blank line

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -102,7 +102,6 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-
     def test_retrieve_user_unauthorized(self):
         """test authentication is required for users"""
         res = self.client.get(ME_URL)
